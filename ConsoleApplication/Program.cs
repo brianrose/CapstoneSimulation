76a8f1fdc35aca74a7b6448bd46f5384314cd0b9
Brian Rose <rose.bri@husky.neu.edu>
2016-01-23 10:15:01 -0500

    Initial commit

---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using Simulation;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int wavelength; // Arg 0
+
+                if (!args.Any())
+                {
+                    // TODO: Default behavior or prompt
+                    wavelength = 780; // default for now
+                }
+                else
+                {
+                    if (!int.TryParse(args?[0], out wavelength))
+                    {
+                        throw new ArgumentException(
+                            "Wavelength could not be parsed. Please enter a valid wavelength in nm.", nameof(wavelength));
+                    }
+                }
+
+                var result = new Simulator().Simulate(wavelength);
+
+                if (result)
+                {
+                    Console.WriteLine("Simulation completed successfully.");
+                }
+                else
+                {
+                    Console.WriteLine("There was a problem running a simulation.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("There was an exception.");
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                Console.WriteLine("ENTER to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Simulation/Simulator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Simulation
+{
+    public class Simulator : ISimulator
+    {
+        public bool Simulate(int wavelength)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
