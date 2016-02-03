90d985ca69388d43fda008a089e7e10147089575
Brian Rose <rose.bri@husky.neu.edu>
2016-02-02 22:42:24 -0500

    Forgot some files

---
+++ SimulationEngine/Converter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimulationEngine
+{
+    public class Converter : IConverter
+    {
+        public TimeSpan ParseTimeSpan(string s)
+        {
+            double time = 0;
+            if (s.Contains("e"))
+            {
+                var split = s.Split('e');
+                time = (double.Parse(split[0])*Math.Pow(10, double.Parse(split[1])));
+            }
+            return TimeSpan.FromSeconds(time);
+        }
+    }
+}
