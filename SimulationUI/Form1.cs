f013b3c1945aa196f625d258f59ce922441964ce
Brian Rose <rose.bri@husky.neu.edu>
2016-02-02 22:41:00 -0500

    Completed ability to add simulations

--- SimulationUI/Form1.cs
+++ SimulationUI/Form1.cs
@@ -17,12 +17,22 @@ namespace SimulationUI
 {
     public partial class Form1 : Form, IView, IModelObserver
     {
+        private int simulationCount = 0;
+        private string simulationsDirectory = "simulations/";
+
+        private IFileSystemAgent fileSystemAgent;
+        private IPhysicsEquationsProvider physicsEquationsProvider;
+        private IConverter converter;
+
         private IController controller;
         public event ViewHandler<IView> Changed;
 
         public Form1()
         {
             InitializeComponent();
+            this.fileSystemAgent = new FileSystemAgent();
+            this.physicsEquationsProvider = new PhysicsEquationsProvider();
+            this.converter = new Converter();
         }
 
         public void SetController(IController controller)
@@ -36,16 +46,29 @@ namespace SimulationUI
 
             foreach (var simulation in e.Simulations)
             {
-                this.listView1.Items.Add(new ListViewItem());
+                ListViewItem row = new ListViewItem(simulation.Id.ToString());
+                string specificationsDisplay = simulation.Specifications.Aggregate("", (current, specification) => current + $"{{{specification.Display}}}");
+                row.SubItems.Add(new ListViewItem.ListViewSubItem(row, specificationsDisplay));
+                listView1.Items.Add(row);
             }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            var specifications = new List<ISpecification>()
+            var id = this.simulationCount;
+            this.simulationCount++;
+            var ledPosition = new Position(double.Parse(sourceXTextBox.Text), double.Parse(sourceYTextBox.Text),
+                double.Parse(sourceZTextBox.Text));
+            var detectorPosition = new Position(double.Parse(detectorXTextBox.Text), double.Parse(detectorYTextBox.Text),
+                double.Parse(detectorZTextBox.Text));
+            var simulationDirectory = $"{this.simulationsDirectory}/{id}";
+            var specifications = new ISpecification[]
             {
-                // TODO: Specifications
+                new LedSpecification(int.Parse(wavelengthTextBox.Text), int.Parse(powerTextBox.Text), ledPosition, simulationDirectory, this.physicsEquationsProvider, this.fileSystemAgent),
+                new DetectorSpecification(int.Parse(radiusTextBox.Text), detectorPosition, simulationDirectory, this.fileSystemAgent),
+                new TimeSpecification(converter.ParseTimeSpan(timeTextBox.Text), simulationDirectory, this.fileSystemAgent)
             };
+            this.controller.AddSimulation(new Simulation(id, specifications));
         }
     }
 }
